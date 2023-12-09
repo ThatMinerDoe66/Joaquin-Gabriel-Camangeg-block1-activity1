7bc5501a9d55df4b037bcfedc9993131b1a3208e
Joaquin Gabriel Camangeg <joaquingabrielcamangeg6@gmail.com>
2023-12-09 21:49:11 +0800

    >Added new page >Added Variables to MainActivity.kt

--- app/src/main/java/com/joaquingabriel/camangeg/block1/p1/quiz/MainActivity.kt
+++ app/src/main/java/com/joaquingabriel/camangeg/block1/p1/quiz/MainActivity.kt
@@ -1,12 +1,29 @@
 package com.joaquingabriel.camangeg.block1.p1.quiz
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var firstPass: EditText
+    private lateinit var secondPass: EditText
+    private lateinit var thirdPass: EditText
+
+    private lateinit var checkAnswers: Button
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        firstPass = findViewById(R.id.firstPass)
+        secondPass = findViewById(R.id.secondPass)
+        thirdPass = findViewById(R.id.thirdPass)
+
+        checkAnswers = findViewById(R.id.checkAnswers)
+
+        checkAnswers.setOnClickListener{
+            val intent = Intent(this,Main
 
 
     }
